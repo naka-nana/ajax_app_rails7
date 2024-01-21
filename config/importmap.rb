33503cfb7bd27fda1517479c7230f1dcdc9b891c
naka-nana <harunanapanda@icloud.com>
2024-01-21 14:49:54 +0900

    エラー修正

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,4 +7,3 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "post", to: "post.js"
 pin "count", to: "count.js"
-https://master.tech-camp.in/v2/curriculums/8390#:~:text=import%20%22post%22-,import%20%22count%22,-app/javascript/count
